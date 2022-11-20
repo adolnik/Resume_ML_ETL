f85194eb2b9871d0279e5450c3e2f4e44e39e5a8
Alexander Dolnik <alexanderdolnik@cloud.upwork.com>
2022-11-20 15:23:46 +0200

    Added Airflowscript

---
+++ airflow_dags/README.md
@@ -0,0 +1,4 @@
+Run airflow
+
+*) Change variable list according to `variables.json`
+*) Do import of `variable.json` (Airflow WebUI: Admin->Variables)
---
+++ airflow_dags/pentaho_bulk_load.py
@@ -0,0 +1,60 @@
+# -*- coding: utf-8 -*-
+"""Example usage"""
+
+from datetime import timedelta
+
+from airflow import DAG
+from airflow.utils.dates import days_ago
+
+from airflow.models import Variable
+from airflow.operators.bash import BashOperator
+from airflow.operators.empty import EmptyOperator
+from airflow_pentaho.operators.kettle import KitchenOperator
+#from airflow_pentaho.operators.kettle import PanOperator
+#from airflow_pentaho.operators.carte import CarteJobOperator
+#from airflow_pentaho.operators.carte import CarteTransOperator
+
+DAG_NAME = 'pentaho_bulk_load'
+DEFAULT_ARGS = {
+    'owner': 'Airflow',
+    'depends_on_past': False,
+    'start_date': days_ago(2),
+    'email': ['a.dolnik@mellivorasoft.com'],
+    'retries': 3,
+    'retry_delay': timedelta(minutes=10),
+    'email_on_failure': False,
+    'email_on_retry': False
+}
+
+default_db_host = Variable.get("DEFAULT_DB_HOST")
+default_db_name = Variable.get("DEFAULT_DB_NAME")
+default_db_pwd = Variable.get("DEFAULT_DB_PWD")
+default_db_user = Variable.get("DEFAULT_DB_USER")
+default_db_pmplan_media_path = Variable.get("DEFAULT_PMPLAN_MEDIA_PATH")
+
+with DAG(dag_id=DAG_NAME,
+         default_args=DEFAULT_ARGS,
+         dagrun_timeout=timedelta(hours=2),
+         schedule_interval='30 0 * * *') as dag:
+
+    # [START check default_db_host]
+    run_default_db_host = BashOperator(
+        task_id='check_default_db_host',
+        bash_command="echo '{}'".format(default_db_host),
+    )
+    # [END check default_db_host]
+
+    job1 = KitchenOperator(
+        dag=dag,
+        task_id='job1',
+        xcom_push=True,
+        directory='/home/pentaho_scripts',
+        job='main',
+        params={
+        'DB_HOST' : "'{}'".format(default_db_host), 
+        'DB_NAME' : "'{}'".format(default_db_name), 
+        'DB_PWD'  : "'{}'".format(default_db_pwd), 
+        'DB_USER' : "'{}'".format(default_db_user), 
+        'default_path': "'{}'".format(default_db_pmplan_media_path)})
+        
+    run_default_db_host >> job1
